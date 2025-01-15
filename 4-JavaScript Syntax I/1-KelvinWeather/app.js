0ad4d89912c96057672418ce1dbe9941634be537
Ferran Bals <ferranbals@gmail.com>
2025-01-15 16:53:21 +0100

    Given context to the projects.

--- 4-JavaScript Syntax I/1-KelvinWeather/app.js
+++ 4-JavaScript Syntax I/1-KelvinWeather/app.js
@@ -1,3 +1,9 @@
+/*
+In this project, we’re going to practice variables and strings in JavaScript so you can hone 
+your skills and feel confident taking them to the real world. Why? 
+These concepts will help you use your data to output information to your users.
+*/
+
 // Forecast of today in kelvin degrees
 const kelvin = 293;
 
